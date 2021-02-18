40528288423eee9c4e2dd1340a2c39c503d09446
Noah172 <jsebastian.garcia1995@gmail.com>
2021-02-17 23:17:13 -0500

    added: pause canvas and win canvas

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button Options;
+    public Button exit;
+    public void LevelSelect(int level)
+    {
+            
+    }
+    
+}
