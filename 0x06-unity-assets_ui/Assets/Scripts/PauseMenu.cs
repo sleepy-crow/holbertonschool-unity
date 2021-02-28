84ef617906fe398e2e9878ff39011aa0c7ebb12b
Noah172 <jsebastian.garcia1995@gmail.com>
2021-02-27 19:49:58 -0500

    Task 0: first commit

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject canv;
+    public bool act = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canv.activeSelf)
+                Resume();
+            else
+                Pause();
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        canv.SetActive(true);
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        canv.SetActive(false);
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Options()
+    {
+        Time.timeScale = 1;
+        PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("Options");
+    }
+}
---
+++ 0x07-unity-animation/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WinMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("currentScene", SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Next()
+    {
+        if (PlayerPrefs.GetInt("currentScene") < 4)
+            SceneManager.LoadScene(PlayerPrefs.GetInt("currentScene") + 1);
+        else
+            SceneManager.LoadScene(0);
+    }
+}
